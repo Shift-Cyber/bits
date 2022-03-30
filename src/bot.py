88c67ea1f1325f74f0b39821dc56d0286082c55d
valkerse <valkerse@kali.lan>
2022-03-30 16:59:08 -0400

    Removed Cog functions and imported all commands from separate .py files Confirmed all commands function this way before committing, then deleted cog files

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,21 @@
 import discord
 import os
+import sys
 from discord.ext import commands
+
+sys.path.insert(0, "/opt/bits/src/commands")
+from alive_command import alive_command
+from whoami_command import whoami_command
+from admin_debug_command import admin_debug_command
 
 class Bot:
     def __init__(self, logging:object, config:object) -> None:
         self.log = logging
         self.config = config
-            
+        adminCheck = self.config.data['bot_settings']['adminRoleId']
+        
+        os.chdir(r"/opt/bits/")
+
         #Configure bot
             #TODO set from configuration, any intents we might need. Can also set contexts in configuration and do it that way
         intents = discord.Intents(messages=True, guilds=True, members=True) 
@@ -15,36 +24,35 @@ class Bot:
 
         self.bot = commands.Bot(command_prefix='!', description=description, intents=intents)
        
+        def is_admin(ctx):
+            commander = ctx.guild.get_member(ctx.author.id)
+            self.log.debug(f"[{commander} role is {commander.top_role}]")
+            return commander.top_role.id == adminCheck
+
         #Logs the bot starting up
         @self.bot.event
         async def on_ready():
             self.log.info(f'Logged in as {self.bot.user} (ID: {self.bot.user.id})')
             print('Bot has been started')
-
-        #Creates !load command that is used to load cog files. All commands are written in cog files, not in this file
-        @self.bot.command()
-        async def load(ctx, extension):
-            self.bot.load_extension(f'cogs.{extension}')
-
-        #Creates !unload command that is used to unload cog files.
-        #Unloading and reloading adds in new commands/changes without having to restart the bot
-        @self.bot.command()
-        async def unload(ctx, extension):
-            self.bot.unload_extension(f'cogs.{extension}')
         
-        #TODO Create reload command that unloads and loads all currently loaded cog files.
+        #Verifies bot is online
         @self.bot.command()
-        async def reload(ctx, extension):
-            pass
+        async def alive(ctx):
+            await alive_command(ctx, self)
+            return
 
+        #Returns Username and Top Role
+        @self.bot.command()
+        async def whoami(ctx):
+            await whoami_command(ctx, self)
+            return
 
-        os.chdir(r"/opt/bits")
-
-        #Gets all .py files in the ./src/cogs folder and loads them automatically on start
-        #TODO Change if statement to get specific files. Not secure to get all .py files
-        #for filename in os.listdir('./cogs'):
-         #   if filename.endswith('.py'):
-          #      self.bot.load_extension(f'cogs.{filename[:-3]}')
+        #TODO Delete this / Used for testing commands.check
+        @commands.check(is_admin) 
+        @self.bot.command()
+        async def admin_debug(ctx):
+            await admin_debug_command(ctx, self)
+            return
 
         #Initialize
         self.__start_bot()
---
+++ src/commands/admin_debug_command.py
@@ -0,0 +1,3 @@
+async def admin_debug_command(ctx, self):
+    adminReturn = "You are Admin"
+    await ctx.send(adminReturn)
