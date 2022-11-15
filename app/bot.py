a1ef8970d889404ec0bf2389264a3ae73efea52f
Nathaniel Singer <nathaniel@singer.cloud>
2022-11-15 02:15:40 -0800

    readme updates

--- app/bot.py
+++ app/bot.py
@@ -6,6 +6,7 @@ import logging
 # third party imports
 import discord
 from discord.ext import commands
+import google.cloud.logging
 
 # local imports
 from cogs.registration import Registration
@@ -14,14 +15,14 @@ from cogs.registration import Registration
 TOKEN = os.environ.get("DISCORD_TOKEN", None)
 VERSION = os.environ.get("VERSION", None)
 
-# configure logging
+# configure logging to GCP
+google.cloud.logging.Client().setup_logging()
+
 logging.basicConfig(
-    stream=sys.stdout,
     level=logging.DEBUG,
     format='%(asctime)s.%(msecs)03d (%(levelname)s | %(filename)s:%(lineno)d) - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
-
 
 # instantiate bot
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix='!')
